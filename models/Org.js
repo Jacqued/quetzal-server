cfbc192da8fc64d8024b681e36eca5d80cc6b27c
Mehdi Benadda <me@mbenadda.com>
2014-04-08 23:20:42 +0200

    Added receivers to Org, remove useless req

--- models/Org.js
+++ models/Org.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var Account = require('./Account').schema;
 
 var Org = new Schema({
 	name: String,
 	created: Date,
 	admins: [{type: Schema.Types.ObjectId, ref: 'Account'}],
-	members: [{type: Schema.Types.ObjectId, ref: 'Account'}]
+	members: [{type: Schema.Types.ObjectId, ref: 'Account'}],
+	receivers: [{type: Schema.Types.ObjectId, ref: 'Receiver'}]
 });
 
 module.exports = {
